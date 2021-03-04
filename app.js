a19d291a1c98419ebd291213fb06a7bc8c99dbd0
YouDontKnowThis <fox.physics@outlook.fr>
2021-03-04 20:41:38 +0100

    working on VR update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const button = document.getElementById("btn");
 const scene = document.querySelector("a-scene");
+const move = document.querySelector("a-sky");
 
 button.addEventListener("click", (e) => {
   scene.enterVR();
